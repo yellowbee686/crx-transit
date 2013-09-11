ed08bab505907914d31e9cf3e98b522f46130fda
greatghoul <greatghoul@gmail.com>
2013-09-12 00:22:52 +0800

    实现简单德服务器词条推送

--- transit/js/event.js
+++ transit/js/event.js
@@ -1,33 +1,57 @@
 var API_URL = 'http://fanyi.youdao.com/openapi.do?keyfrom=TransIt&key=597592531&type=data&doctype=json&version=1.1&q='
 var PUSH_URL = 'http://trit.herokuapp.com/api/items'
+var TRANSIT_ERROR = '<div class="transit-error">未找到释义</div>';
 
-// Push translation to server.
+// 推送词条到服务器
+// TODO: 实现用户登录功能，将词条推送到自己的账户下
 function pushItem(name, explaination) {
-    var params = Object.toQueryString({ name: name, explaination: explaination})
+    var params = Object.toQueryString({ name: name, explaination: explaination});
+    console.log("  Pushing translation to server:", params);
     var xhr = new XMLHttpRequest();
     xhr.open('POST', PUSH_URL, true);
+    xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+    xhr.setRequestHeader("Content-length", params.length);
+    xhr.setRequestHeader("Connection", "close");
     xhr.send(params);
 }
 
 
-// Message Passing Listeners
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.type == 'translation') {
-        console.log('Translating text:', request.text);
+// 执行翻译动作
+function translateHanlder(request, sender, sendResponse) {
+    console.log('Translating text:', request.text);
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                var result = JSON.parse(this.responseText);
-                console.log('==>', result);
-                if (!result.errorCode) {
-                    sendResponse({ result: result });
-                }
-            }
-        };
-        xhr.open('GET', API_URL + encodeURIComponent(request.text), true);
-        xhr.send();
-    }
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState != 4) return;
+
+        var result = JSON.parse(this.responseText);
+        console.log('==>', result);
+        
+        if (!result || result.errorCode) return;
+
+        var translation = getTranslation(result);
+
+        if (translation) {
+            sendResponse({ translation: translation });
+            pushItem.delay(100, request.text, translation);
+        } else {
+            sendResponse({ translation: TRANSIT_ERROR });
+        }
+        
+    };
+    xhr.open('GET', API_URL + encodeURIComponent(request.text), true);
+    xhr.send();
+}
+
+
+var dispatcher = {
+    translate: translateHanlder
+};
+
+// 响应来自页面和弹出层的翻译请求
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var handler = dispatcher[request.type] || noop;
+    handler(request, sender, sendResponse);
 
     return true;
 });
--- transit/js/helpers.js
+++ transit/js/helpers.js
@@ -5,19 +5,25 @@ function strip(s) {
     return (s || '').replace(/(^\s+|\s+$)/g, '');
 }
 
+
 // 从查询结果中提取出翻译结果
 function getTranslation(result) {
-    var translation = null; 
-    if (result.basic) {
-        translation = result.basic.explains.join('<br/>');
-    } else if (result.translation) {
-        translation = result.translation.join('<br />');
+    var translation = null;
+    if (result && !result.errorCode) {
+        if (result.basic) {
+            translation = result.basic.explains.join('<br/>');
+        } else if (result.translation) {
+            translation = result.translation.join('<br />');
+        }
+        
+        if (translation == result.query) {
+            translation = null;
+        }
     }
-    
-    if (translation != result.query) {
-        return translation;
-    } else {
-        return '<div class="transit-error">未查询到释义</div>';
-    }
+
+    return translation;
 }
 
+
+// 空方法，用来处理无效的回调
+function noop() {}
