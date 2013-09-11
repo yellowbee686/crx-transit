84862bd33b28af182fae17381888503cb2da7992
greatghoul <greatghoul@gmail.com>
2013-09-11 18:25:26 +0800

    Start to push service

--- transit/js/event.js
+++ transit/js/event.js
@@ -1,4 +1,14 @@
 var API_URL = 'http://fanyi.youdao.com/openapi.do?keyfrom=TransIt&key=597592531&type=data&doctype=json&version=1.1&q='
+var PUSH_URL = 'http://trit.herokuapp.com/api/items'
+
+// Push translation to server.
+function pushItem(name, explaination) {
+    var params = Object.toQueryString({ name: name, explaination: explaination})
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', PUSH_URL, true);
+    xhr.send(params);
+}
+
 
 // Message Passing Listeners
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -10,7 +20,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             if (this.readyState == 4) {
                 var result = JSON.parse(this.responseText);
                 console.log('==>', result);
-                result.errorCode || sendResponse({ result: result });
+                if (!result.errorCode) {
+                    sendResponse({ result: result });
+                }
             }
         };
         xhr.open('GET', API_URL + encodeURIComponent(request.text), true);
