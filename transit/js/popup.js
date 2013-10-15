b1b6c851fd4ea137027987b13fecbfec6a93ddd7
greatghoul <greatghoul@gmail.com>
2013-10-15 09:54:31 +0800

    修复弹出层中输入单词查询无响应的问题
    移除了 sugarjs 后没有修改相应的代码，现在已经使用 jQuery 替换了 sugarjs

--- transit/js/popup.js
+++ transit/js/popup.js
@@ -1,38 +1,37 @@
-var app    = chrome.extension.getBackgroundPage(),
-	source = document.getElementById('source'),
-    result = document.getElementById('result'),
-    toggle = document.getElementById('toggle');
+$(function() {
+    var app = chrome.extension.getBackgroundPage();
+	var $source = $('#source');
+    var $result = $('#result');
+    var $toggle = $('#toggle');
 
-// 查询单词
-function transit(evt) {
-	if (evt.keyCode != 13) return;
-	if (source.value.isBlank()) return;
+    // 查询单词
+    function transit(evt) {
+        var text = $.trim($source.val());
 
-    chrome.extension.sendMessage({ type: 'translate', text: source.value.trim() }, function(response) {
-        result.innerHTML = response.translation;
-    });
+        if (evt.keyCode != 13) return;
+        if (!text) return;
 
-    return false;
-}
+        chrome.extension.sendMessage({ type: 'translate', text: text }, function(response) {
+            $result.html(response.translation);
+        });
 
-// 启用和禁用页面划词
-function togglePageSelection() {
-	app.settings(this.name, this.checked);
-}
+        return false;
+    }
 
+    // 启用和禁用页面划词
+    function togglePageSelection() {
+        app.settings(this.name, this.checked);
+    }
 
-function initialize() {
-	// 事件注册
-	source.addEventListener('keypress', transit, false);
-	toggle.addEventListener('change', togglePageSelection, false);
+    // 事件注册
+    $source.on('keypress', transit);
+    $toggle.on('change', togglePageSelection);
 
-	// 读取初始配置
-	if (app.settings(toggle.name) == null) {
-		app.settings(toggle.name, true);
-	}
+    // 读取初始配置
+    if (app.settings(toggle.name) == null) {
+        app.settings(toggle.name, true);
+    }
 
-	toggle.checked = app.settings(toggle.name); 
-	source.focus();
-}
-
-initialize();
+    $toggle.prop('checked', app.settings(toggle.name)); 
+    $source.focus();
+});
