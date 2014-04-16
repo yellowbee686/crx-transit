8a513a8b35ecdc61a6650059956e066e7a14a0bd
greatghoul <greatghoul@gmail.com>
2014-04-16 22:33:13 +0800

    补上漏删除的代码

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,3 +1,32 @@
+var app = chrome.extension.getBackgroundPage(),
+    $source = $('#source'),
+    $result = $('#result');
+
+function translate(text) {
+    if (!text) return;
+
+    chrome.extension.sendMessage({ type: 'translate', text: text }, 
+        function(response) {
+            $result.html(response.translation);
+        }
+    );
+}
+
+function transit(evt) {
+    var text = $source.val().trim();
+
+    if (evt.keyCode != 13) return;
+
+    translate(text);
+
+    return false;
+}
+
+$source.on('keypress', transit);
+$source.focus();
+$source.val(app.currentText);
+translate(app.currentText);
+
 var app = angular.module('TransitPopupApp', []);
 app.controller('OptionsCtrl', function($scope, $window) {
     initOptions(function() {
@@ -11,5 +40,7 @@ app.controller('OptionsCtrl', function($scope, $window) {
         for (var name in options) {
             $scope.$watch(fmt('options.#{1}', name), saveOptions);
         }
+
+
     });
 }); 
