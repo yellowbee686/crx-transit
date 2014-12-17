84b84bd93da79a7bc7c7ebbc002e1e2d3d374c89
greatghoul <greatghoul@gmail.com>
2014-12-17 20:28:07 +0800

    use capslock double key to toggle link inspect

--- extension/js/action.user.js
+++ extension/js/action.user.js
@@ -40,57 +40,24 @@ function transIt(evt) {
     }
 }
 
-function focusLink(evt) {
-    if (options.linkInspect) {
-        evt.stopPropagation();
+var capslockEvents = [];
+function toggleLinkInspectMode(evt) {
+    if (!(options.linkInspect && evt.keyCode == 20)) return;
 
-        $link = $(this);
-        evt.shiftKey && disableLink(evt);
-    }
-}
-
-function blurLink(evt) {
-    if (options.linkInspect) {
-        evt.stopPropagation();
-
-        if ($link) {
-            if ($link.hasClass('transit-link')) {
-                enableLink(evt, true);
-            }
-        }
-
-        $link = null;
-    }
-}
-
-// 暂时清除链接地址，以便对链接进行划词
-function disableLink(evt) {
-    if ($link && evt.shiftKey) {
-        $link.data('transit-href', $link.attr('href')).removeAttr('href').addClass('transit-link');
-    };
-}
-
-// 复原链接地址
-function enableLink(evt, ignoreKey) {
-    if ($link && (ignoreKey || evt.keyCode == 16)) {
-        $link.attr('href', $link.data('transit-href')).removeClass('transit-link');
-    }
-}
-
-// 清除选择
-function clearSelection(evt) {
-    // 当使用 Shift 作为辅助键时，清空之前的选择
-    if (options.linkInspect && evt.button == 0 && evt.shiftKey) {
-        window.getSelection().empty();
+    capslockEvents.push(evt.keyCode);
+    if (capslockEvents.length == 1) {
+        var timer = setTimeout(function() {
+            capslockEvents = [];
+            clearTimeout(timer);
+        }, 500);
+    } else {
+        $('body').toggleClass('transit-link-inspect-mode');
+        capslockEvents = [];
     }
 }
 
 initOptions(function(options) {
+    $(document).on('keyup keydown', toggleLinkInspectMode);
     $(document).on('mouseup', transIt);
-    $(document).on('mouseenter', 'a', focusLink);
-    $(document).on('mouseleave', 'a', blurLink);
-    $(document).on('keydown', disableLink);
-    $(document).on('keyup', enableLink);
-    $(document).on('mousedown', clearSelection);
     log('action.user.js initialized.')
 });
